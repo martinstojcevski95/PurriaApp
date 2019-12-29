4647193664429e5eda7f48d8785d08817176c72c
martinstojcevski95 <stojcevski.m95@gmail.com>
2019-12-29 14:20:18 +0100

    Latest update
    Latest update

---
+++ Assets/Scripts/PlantForContractTwo.cs
@@ -0,0 +1,100 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantForContractTwo : MonoBehaviour
+{
+    public SinglePlantTwo plant;
+    public int id;
+    public int ContractID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(DATABASEURL);
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    // BEZ OBIEKTI DA PROBMSA D AZAPISAM WTFFF
+    IEnumerator PlantingThePlant(int contract)
+    {
+        ContractID = contract;
+        yield return new WaitForSeconds(1f);
+        plant = new SinglePlantTwo();
+        plant.ContractID = ContractID;
+        plant.isPlantInContract = true;
+        plant.isPlantPlanted = true;
+        plant.isDroneAssigned = false;
+        plant.FieldID = id;
+        //ContractID = contract;
+       // PlayerPrefs.SetInt("contract", ContractID);
+
+        string ToJson = JsonUtility.ToJson(plant);
+        reference.Child("USERS").Child(LogInAndRegister.Instance.UserName).Child("GAMESPACE").Child("CONTRACT" + ContractID).Child("PLANTS").Child("PLANT" + plant.FieldID).SetRawJsonValueAsync(ToJson);
+
+      //  PlayerPrefs.SetInt("plantid", id);
+    }
+
+
+    public void GetDBDataForPlant()
+    {
+        FirebaseDatabase.DefaultInstance
+             .GetReference("USERS").Child(LogInAndRegister.Instance.UserName).Child("GAMESPACE").Child("CONTRACT" + ContractID).Child("PLANTS").Child("PLANT" + id)
+             .GetValueAsync().ContinueWith(task =>
+             {
+                 if (task.IsFaulted)
+                 {
+                     // Handle the error...
+                 }
+                 else if (task.IsCompleted)
+                 {
+                     DataSnapshot snapshot = task.Result;
+                  //   Debug.Log("data for plant " + snapshot.GetRawJsonValue());
+                     plant = JsonUtility.FromJson<SinglePlantTwo>(snapshot.GetRawJsonValue());
+                     // StartCoroutine(GetTheDataFromTheDbAndPopulateThePlant(snapshot.GetRawJsonValue()));
+
+                 }
+             });
+    }
+    public void SetEachPlantDataInDbAfterCreatingContract()
+    {
+        plant = new SinglePlantTwo();
+        plant.ContractID = ContractID;
+        plant.isPlantInContract = true;
+        plant.isPlantPlanted = true;
+        plant.isDroneAssigned = false;
+        plant.FieldID = id;
+        //  PlayerPrefs.SetInt("contract", ContractID);
+
+        string ToJson = JsonUtility.ToJson(plant);
+        reference.Child("USERS").Child(LogInAndRegister.Instance.UserName).Child("GAMESPACE").Child("CONTRACT" + plant.ContractID).Child("PLANTS").Child("PLANT" + plant.FieldID).SetRawJsonValueAsync(ToJson);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //PRIVATE VARIABLES
+    DatabaseReference reference;
+    string DATABASEURL = "https://purriadb.firebaseio.com/";
+    int plantID;
+
+    [Serializable]
+    public class SinglePlantTwo
+    {
+        public bool isDroneAssigned;
+        public bool isPlantPlanted;
+        public bool isPlantInContract;
+        public int FieldID;
+        public int ContractID;
+        public int GrowthDays;
+    }
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+
+    public int canCount; //1 = true 0 = false 
+    public float TimePassed; // in seconds
+    public static TimeManager Instance;
+
+
+    void Awake()
+    {
+        Instance = this;
+        TimePassed = PlayerPrefs.GetFloat("TimePassed");
+    }
+
+    void Stat()
+    {
+        canCount = 0;
+
+    }
+
+
+
+    void Update()
+    {
+
+        if (canCount == 1)
+        {
+            TimePassed += Time.deltaTime;
+
+            if (TimePassed > 10) //86400)
+            {
+                // reset time to 0 and save it
+                Debug.Log("day has passed");
+                TimePassed = 0;
+                canCount = 0;
+                PlantsManager.PlantsManagerInstance.IncrementGrowthDaysForEachPlans();
+
+
+            }
+
+        }
+
+    }
+
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.SetFloat("TimePassed", TimePassed);
+    }
+
+
+}
